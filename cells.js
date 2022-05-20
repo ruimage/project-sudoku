d2fd9ba5dfd614dd3e6c4a872dca6428923adc3c
Anton Valkovsky <ruizave@gmail.com>
2022-05-20 15:39:10 +0300

    реализован метод получения массива координат пустых ячеек

--- cells.js
+++ cells.js
@@ -1,5 +1,17 @@
+/* eslint-disable */
 const getEmptyCells = (puzzle) => {
-
+  let arrEmpty = [];
+  for (let i = 0; i < puzzle.length; i++) {
+    if (Array.isArray(puzzle[i])) {
+      for (let j = 0; j < puzzle[i].length; j++) {
+        if ( puzzle[i][j]  == '-'){
+          arrEmpty.push([i,j]);
+        }
+      }
+    }
+  }
+  console.log(arrEmpty);
+  return arrEmpty;
 };
 
 module.exports = { getEmptyCells };
